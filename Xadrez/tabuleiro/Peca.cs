0e541287850113fecbb77fa2edc369c85ad46dad
Rafael Jáber <rafael.jaber@hotmail.com>
2022-11-28 13:22:24 -0300

    Testes iniciais com movimentos possíveis das peças

--- Xadrez/tabuleiro/Peca.cs
+++ Xadrez/tabuleiro/Peca.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Xadrez.tabuleiro
 {
-    internal class Peca
+    abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -28,5 +28,7 @@ namespace Xadrez.tabuleiro
         {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis(); 
     }
 }
