e4d9d91b3970dd20a71480fad3478d5e83d70ed3
Rafael Jáber <rafael.jaber@hotmail.com>
2022-11-28 22:31:52 -0300

    Implementada jogadda especial promoção

--- Xadrez/xadez/PartidaDeXadrez.cs
+++ Xadrez/xadez/PartidaDeXadrez.cs
@@ -110,7 +110,7 @@ namespace Xadrez.xadez
                 tab.colocarPeca(T, origemT);
             }
 
-            // #jogadaespecial en passant
+            // #jogadaEspecial en passant
             if (p is Peao)
             {
                 if (origem.coluna != destino.coluna && pecaCapturada == vulneravelEnPassant)
@@ -140,6 +140,21 @@ namespace Xadrez.xadez
                 throw new TabuleiroException("Você não pode se colocar em cheque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #jogadaEspecial promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -157,6 +172,16 @@ namespace Xadrez.xadez
             {
                 turno++;
                 mudaJogador();
+            }
+
+            // #jogadaEspecial en passant
+            if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
+            {
+                vulneravelEnPassant = p;
+            }
+            else
+            {
+                vulneravelEnPassant = null;
             }
 
 
